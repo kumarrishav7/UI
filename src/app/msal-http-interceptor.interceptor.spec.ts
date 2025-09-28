28c5a75c81c6de7eaf39bcedec73f9d9a3ef8e2c
kumarrishav7 <kumarrishav7@gmail.com>
2025-09-28 13:46:14 +0530

    running tests using chrome-headless

--- src/app/msal-http-interceptor.interceptor.spec.ts
+++ src/app/msal-http-interceptor.interceptor.spec.ts
@@ -1,6 +1,5 @@
 import { TestBed } from '@angular/core/testing';
 import { HttpInterceptorFn } from '@angular/common/http';
-
 import { authInterceptor } from './msal-http-interceptor.interceptor';
 
 describe('authInterceptor', () => {
