5b853e91970b4f9c796217217673408835094069
kumarrishav7 <kumarrishav7@gmail.com>
2025-09-29 11:55:31 +0530

    Added web.config for routing, fixed duplicate appComponent rendering

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,12 +1,11 @@
 import { Routes } from '@angular/router';
-import { MsalGuard } from '@azure/msal-angular';
-import { AppComponent } from './app.component';
 import { ProfileComponent } from './profile/profile.component';
 import { LoginComponent } from './login/login.component';
 import { authGuard } from './auth-guard.guard';
+import { HomeComponent } from './home/home.component';
 
 export const routes: Routes = [
-  { path: '', component: AppComponent },
+  { path: '', component: HomeComponent },
   { path: 'login', component: LoginComponent },
   { path: 'profile', component: ProfileComponent, canActivate: [authGuard] },
 ];
